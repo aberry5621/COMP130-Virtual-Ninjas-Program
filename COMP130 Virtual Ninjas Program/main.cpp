0e60752deddb85af90fb128ddf35a799b44e4217
aberry5621 <aberry5621@gmail.com>
2016-11-30 09:10:29 -0800

    Example sample done

--- COMP130 Virtual Ninjas Program/main.cpp
+++ COMP130 Virtual Ninjas Program/main.cpp
@@ -7,9 +7,47 @@
 //
 
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
+// define classes
+
+class Enemy {
+    public:
+        // polymorphic virtual function will refer to specific classes for method actions
+        virtual void attack() {}
+};
+
+class Ninja: public Enemy {
+    public:
+        void attack() {
+            cout << "karate chop!" << endl;
+        }
+};
+
+class Monster: public Enemy {
+    public:
+        void attack() {
+            cout << "rawr blarrg!" << endl;
+        }
+};
+
+int main() {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    cout << "Virtual Ninjas!\n";
+
+    // create onjects
+    Ninja n; // object n of Ninja data type
+    Monster m; // object m of Monster data type
+    Enemy * enemy1 = &n; // enemy 1 pointer set to address of ninja object
+    Enemy * enemy2 = &m; // enemy 2 pointer set to address of monster object
+    
+    // call functions by reference (*this is necessary)
+    (*enemy1).attack();
+    (*enemy2).attack();
+    
+    // or use convenient arrow operators
+    enemy1->attack();
+    enemy2->attack();
+    
     return 0;
 }
