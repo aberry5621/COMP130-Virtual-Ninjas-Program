8e7f095304219baa08a6319cb9a383b177ea50ca
aberry5621 <aberry5621@gmail.com>
2016-11-30 08:53:51 -0800

    Initial Commit

---
+++ COMP130 Virtual Ninjas Program/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  COMP130 Virtual Ninjas Program
+//
+//  Created by ax on 11/30/16.
+//  Copyright © 2016 COMP130. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
